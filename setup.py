7c35534c3ad5556a3f4edb8aa75268d6b37c2190
Noah <noah.inada@docmapping.com>
2020-06-05 11:00:39 -0700

    _

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GBM-and-LGG',
+      version='1.0',
+      description='DSC 180 Senior Capstone Project',
+      author='Genetics - Group 2',
+      author_email='ninada@ucsd.edu',
+      url='https://github.com/Ninada-U/Analyzing-CNVs-between-GBM-and-LGG',
+      packages=['pandas', 'subprocess', 'matplotlib', 're'],
+     )
